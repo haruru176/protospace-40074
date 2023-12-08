6766b8bd3d493544753380b23c846f667f807307
21shige <146493286+21shige@users.noreply.github.com>
2023-12-08 10:19:55 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get 'prototypes/index'
+  root to: "prototypes#index"
+  resources :prototypes do
+    resources :comments, only: [:index, :create]
+  end
+  resources :users, only: [:show]
+end
+
+
+# resources :prototypes do
+#   resources :comments, only: [:index, :create]
+# end
