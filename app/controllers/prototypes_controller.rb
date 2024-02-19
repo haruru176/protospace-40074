a88416e8497a64a0fc58fffd58637dde9f4d230f
haruru176 <157213835+haruru176@users.noreply.github.com>
2024-02-19 17:57:52 +0900

    master

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only: [:edit, :show, :new, :destroy]
+  before_action :authenticate_user!, only: [:edit, :new, :destroy]
 
   def index
     @prototypes = Prototype.all
